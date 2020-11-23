e0b70b7a3c8eea0b0386baaecf0e05160b360090
Nazarii Kuspys <nazariikuspys@pop-os.localdomain>
2020-11-23 20:41:19 +0200

    fixed typo

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,6 +1,6 @@
 package ua.edu.ucu.collections;
 
-import ua.edu.ucu.collections.immutable.
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
 public class Stack {
     private ImmutableLinkedList lst;
 
